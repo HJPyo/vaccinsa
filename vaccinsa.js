dced41a2c8b9bb1d55d6c9aa22e3cda5f0eaa0b8
HJPyo <bunker12383@gmail.com>
2021-07-14 01:04:56 +0900

    4.1
    4.1

--- vaccinsa.js
+++ vaccinsa.js
@@ -1,3 +1,16 @@
+function makemap() {
+	var mapContainer = document.getElementById('map'),
+		mapOption = {
+			center: new kakao.maps.LatLng(36.33085, 127.80683),
+			level: 13,
+			mapTypeId: kakao.maps.MapTypeId.ROADMAP
+		};
+
+	var map = new kakao.maps.Map(mapContainer, mapOption);
+}
+
+makemap();
+
 var url = "https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=512&serviceKey=4llzQmwfDhsCiVmcR2GcPB827zs0s0OiKwizB3GV21WblIjHwh3DcB35Y%2F0Tw2cnMqFebNscjA%2FuNucdQnetuA%3D%3D";
 var mydata;
 
@@ -12,12 +25,3 @@ fetch(url)
 	.catch(function (err) {
 		console.log(err);
 	});
-
-var mapContainer = document.getElementById('map'),
-	mapOption = {
-		center: new kakao.maps.LatLng(36.33085, 127.80683),
-		level: 13,
-		mapTypeId: kakao.maps.MapTypeId.ROADMAP
-	};
-
-var map = new kakao.maps.Map(mapContainer, mapOption);
