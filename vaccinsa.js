c66ef097d595e09a9c78a5365a187fb16567d7ca
HJPyo <bunker12383@gmail.com>
2021-07-14 01:29:33 +0900

    6.1
    6.1

--- vaccinsa.js
+++ vaccinsa.js
@@ -1,6 +1,8 @@
 var url = "https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=512&serviceKey=4llzQmwfDhsCiVmcR2GcPB827zs0s0OiKwizB3GV21WblIjHwh3DcB35Y%2F0Tw2cnMqFebNscjA%2FuNucdQnetuA%3D%3D";
 var mydata;
 
+console.log("UPDATED");
+
 fetch(url)
 	.then(function (res) {
 		return res.json();
@@ -20,14 +22,12 @@ fetch(url)
 
 		var mark = [];
 
-		for(var i = 0; i < 280; i++){
+		for (var i = 0; i < 280; i++) {
 			mark.push = new kakao.maps.Marker({
 				position: new kakao.maps.LatLng(Number(mydata[i].lat), Number(mydata[i].lng)),
 				map: map
 			});
 		}
-
-		console.log("UPDATED");
 	})
 	.catch(function (err) {
 		console.log(err);
